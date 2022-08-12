d70d37d3dc792742cbc4f6fe4dd4d51243c32793
fish <fisseha.137@gmail.com>
2022-08-12 12:11:45 +0300

    data handler: inserted a dataframe insert method

--- data_handler.py
+++ data_handler.py
@@ -51,6 +51,40 @@ def execute_query(connection: sqlite3.Connection, query:str) -> None:
         print(f"The error '{e}' occurred")
 
 
+def insert_to_tweet_table(connection: sqlite3.Connection, df: pd.DataFrame, table_name: str) -> None:
+    """
+    A method to insert dataframe data into a data base
+
+    Parameters
+    ----------
+    connection :
+        sqlite3.Connection : the database connection
+    df :
+        pd.DataFrame : the dataframe
+    table_name :
+        str : the tablename
+    Returns :
+        nothing
+    -------
+    """
+    for _, row in df.iterrows():
+        sqlQuery = f"""INSERT INTO {table_name} (statuses_count, created_at, source, original_text, polarity,
+        subjectivity, favorite_count, retweet_count, screen_name, followers_count, friends_count, possibly_sensitive, hashtags, user_mentions, location,language)
+             VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);"""
+        data = (row[0], row[1], row[2], row[3], (row[4]), (row[5]), row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[13], row[14], row[15])
+
+        try:
+            cur = connection.cursor()
+            # Execute the SQL command
+            cur.execute(sqlQuery, data)
+            # Commit your changes in the database
+            connection.commit()
+            print("Data Inserted Successfully")
+        except Exception as e:
+            connection.rollback()
+            print("Error: ", e)
+    return
+
 if __name__ == "__main__":
     connection = DBConnect(dbName='tweets.db')
     execute_query(connection=connection, query='create_table.sql')
