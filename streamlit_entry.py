983c4ee7b8d537feca80b69f2bd6b56e0b54dc83
fish <fisseha.137@gmail.com>
2022-08-12 13:08:50 +0300

    streamlit: markdown syntax bug fix

--- streamlit_entry.py
+++ streamlit_entry.py
@@ -104,9 +104,8 @@ def langPie():
 
 st.title("Twitter Data Display")
 selectHashTag()
-st.markdown("<p style='padding:10px; background-color:#000000;color:#00ECB9;font-size:16px;border-radius:10px;'>Section 
 
-Break</p>", unsafe_allow_html=True)
+st.markdown("<p style='padding:10px; background-color:#000000;color:#00ECB9;font-size:16px;border-radius:10px;'>Section Break</p>", unsafe_allow_html=True)
 
 selectLocAndAuth()
 st.title("Data Visualizations")
