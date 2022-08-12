7ca29812c2d54a2c4a407d93469b9622891e40db
fish <fisseha.137@gmail.com>
2022-08-12 13:03:54 +0300

    streamlit: added a loaction and language selctor to load data from data base

--- streamlit_entry.py
+++ streamlit_entry.py
@@ -14,6 +14,7 @@ from data_handler import DBConnect
 # set the page and title
 st.set_page_config(page_title="Twitter data analysis", layout="wide")
 
+
 def loadData():
     """
     A method to load the data from our data base
@@ -23,6 +24,7 @@ def loadData():
     df = db_execute_fetch(connection, query, dbName="tweets.db", rdf=True)
     return df
 
+
 def selectHashTag():
     df = loadData()
     hashTags = st.multiselect("choose combination of hashtags", list(df['hashtags'].unique()))
@@ -30,6 +32,24 @@ def selectHashTag():
         df = df[np.isin(df, hashTags).any(axis=1)]
         st.write(df)
 
+
+def selectLocAndAuth():
+    df = loadData()
+    location = st.multiselect("choose Location of tweets", list(df['location'].unique()))
+    lang = st.multiselect("choose Language of tweets", list(df['language'].unique()))
+
+    if location and not lang:
+        df = df[np.isin(df, location).any(axis=1)]
+        st.write(df)
+    elif lang and not location:
+        df = df[np.isin(df, lang).any(axis=1)]
+        st.write(df)
+    elif lang and location:
+        location.extend(lang)
+        df = df[np.isin(df, location).any(axis=1)]
+        st.write(df)
+    else:
+        st.write(df)
 
 
 
